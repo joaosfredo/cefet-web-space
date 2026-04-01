fdf7c97617f683e6c1b459ad4e3b63213f9d89f9
joaosfredo <joaosfredo555@gmail.com>
2026-04-01 19:56:51 -0300

    feat: exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,18 @@
 // Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botoes = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let botao of botoes) {
+  botao.addEventListener('click', function (e) {
+    let botaoClicado = e.currentTarget;
+    let paragrafo = botaoClicado.parentElement;
+
+    paragrafo.classList.toggle('expandido');
+
+    if (paragrafo.classList.contains('expandido')) {
+      botaoClicado.innerHTML = '-';
+    } else {
+      botaoClicado.innerHTML = '+';
+    }
+  });
+}

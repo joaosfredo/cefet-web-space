ea24d4f98585361a094bad7c21c7d3f21e83d370
joaosfredo <joaosfredo555@gmail.com>
2026-04-01 09:52:41 -0300

    feat: exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,20 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botao = document.getElementById("calcular");
+
+botao.addEventListener("click", function () {
+
+  let G = document.getElementById("constante").value;
+  let M1 = document.getElementById("massa1").value;
+  let M2 = document.getElementById("massa2").value;
+  let d = document.getElementById("distancia").value;
+
+  G = Number(G);
+  M1 = Number(M1);
+  M2 = Number(M2);
+  d = Number(d);
+
+  let F = (G * M1 * M2) / (d * d);
+
+  document.getElementById("resultado").value = F;
+});
